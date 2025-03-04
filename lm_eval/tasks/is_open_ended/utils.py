22c0d3aa965467beb9b35e0b230b880c0d7a27c1
elements72 <antonio.lopez.8523@gmail.com>
2025-03-04 15:52:33 +0100

    Added bertscore sub-metric

--- lm_eval/tasks/is_open_ended/utils.py
+++ lm_eval/tasks/is_open_ended/utils.py
@@ -2,6 +2,8 @@ import datasets
 import re
 import evaluate
 
+from scripts.model_comparator import print_results
+
 bertscore = evaluate.load("bertscore")
 
 def process_answer(answer):
@@ -11,6 +13,9 @@ def process_answer(answer):
 
 
 def process_results(doc, results):
+    print("doc:", doc)
+    print("results:", results)
+    results = results[0]
     result = bertscore.compute(predictions=results, references=doc)
 
     return {"bertscore_f1": result['f1'][0], "bertscore_precision": result['precision'][0], "bertscore_recall": result['recall'][0]}
